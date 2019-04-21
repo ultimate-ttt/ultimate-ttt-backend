25a7ae22f5c11097da0bd46cf94881f7aee20169
Florian Rohrer <Florian.Rohrer@vivaldi.net>
2019-04-21 09:49:01 +0200

    Environment variable configuration support

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -45,8 +45,9 @@ namespace UltimateTicTacToe.Api
             services.AddGraphQL(schema.MakeExecutable(c =>
                 c.UseDefaultPipeline(new QueryExecutionOptions
                     {
-                        TracingPreference = TracingPreference.Always,
-                        IncludeExceptionDetails = true
+#if DEBUG
+                        TracingPreference = TracingPreference.Always, IncludeExceptionDetails = true
+#endif
                     })
                     .AddCustomErrorFilters()));
         }
