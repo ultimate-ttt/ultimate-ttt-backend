dcdfaab5ddf2fb562308b80d33afdab068a5f3e7
Florian Rohrer <Florian.Rohrer@vivaldi.net>
2019-05-05 19:17:45 +0200

    BoardWinnerEvaluatorTests GetWinner_EmptyBoard_ReturnsWinnerNone

---
+++ src/Domain.Tests/BoardWinnerEvaluatorTests.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+using UltimateTicTacToe.Abstractions;
+using Xunit;
+
+namespace UltimateTicTacToe.Domain.Tests
+{
+    public class BoardWinnerEvaluatorTests
+    {
+        [Theory]
+        [InlineData(Player.Circle)]
+        [InlineData(Player.Cross)]
+        private void GetWinner_EmptyBoard_ReturnsWinnerNone(Player lastPlayer)
+        {
+            // arrange
+            var emtyBoard =  new SmallTileInformation[3][];
+
+            for (int x = 0; x < 3; x++)
+            {
+                emtyBoard[x] = new SmallTileInformation[3];
+                for (int y = 0; y < 3; y++)
+                {
+                    emtyBoard[x][y] = new SmallTileInformation
+                    {
+                        BoardPosition = new Position(0, 0),
+                        Position = new Position(x, y),
+                        Value = TileValue.Empty,
+                    };
+                }
+            }
+
+            // act
+            var winner = emtyBoard.GetWinner(lastPlayer);
+
+            // assert
+            winner.Should().Be(Winner.None);
+        }
+    }
+}
