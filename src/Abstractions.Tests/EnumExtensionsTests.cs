84e8bb2fff9a850f756dbb7d874598e47f31e0b1
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-05-06 15:45:45 +0200

    EnumExtensionsTests

---
+++ src/Abstractions.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using Xunit;
+
+namespace UltimateTicTacToe.Abstractions.Tests
+{
+    public class EnumExtensionsTests
+    {
+        [Theory]
+        [InlineData(Player.Circle, TileValue.Circle)]
+        [InlineData(Player.Cross, TileValue.Cross)]
+        public void ToTileValue_PlayerInput_ValidOutput(Player input, TileValue expectedOutput)
+        {
+            // arrange
+
+            //act
+            var result = input.ToTileValue();
+
+            //assert
+            result.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(Player.Circle, Winner.Circle)]
+        [InlineData(Player.Cross, Winner.Cross)]
+        public void ToWinner_PlayerInput_ValidOutput(Player input, Winner expectedOutput)
+        {
+            // arrange
+
+            //act
+            var result = input.ToWinner();
+
+            //assert
+            result.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(Player.Circle, Player.Cross)]
+        [InlineData(Player.Cross, Player.Circle)]
+        public void Invert_PlayerInput_ValidOutput(Player input, Player expectedOutput)
+        {
+            // arrange
+
+            //act
+            var result = input.Invert();
+
+            //assert
+            result.Should().Be(expectedOutput);
+        }
+    }
+}
