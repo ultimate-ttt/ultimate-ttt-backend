b2a1aae4c897d4bd31669d21fd878b88534b70ed
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-05-07 15:29:28 +0200

    MoveValidatorTests

---
+++ src/Domain.Tests/MoveValidatorTests.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using UltimateTicTacToe.Abstractions;
+using UltimateTicTacToe.Data.Abstractions;
+using Xunit;
+
+namespace UltimateTicTacToe.Domain.Tests
+{
+    public class MoveValidatorTests
+    {
+        [Fact]
+        public async Task ValidateMoveAsync_FirstMove_CorrectMoveNumber()
+        {
+            // arrange
+            Mock<IMoveRepository> moveRepositoryMock = new Mock<IMoveRepository>();
+            moveRepositoryMock
+                .Setup(m => m.GetMovesForGame(It.IsAny<string>(), CancellationToken.None))
+                .Returns(() => Task.FromResult(new List<Move>()))
+                .Verifiable();
+
+            var gameManager = new MoveValidator(moveRepositoryMock.Object);
+
+            // act
+            var result = await gameManager.ValidateMoveAsync(
+                new Move
+                {
+                    GameId = "abc",
+                    BoardPosition = new Position {X = 0, Y = 0},
+                    TilePosition = new Position {X = 0, Y = 0},
+                    Player = Player.Cross
+                }, CancellationToken.None);
+
+            // assert
+            result.Move.MoveNumber.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task ValidateMoveAsync_ValidMove_CorrectMoveNumber()
+        {
+            // arrange
+            Mock<IMoveRepository> moveRepositoryMock = new Mock<IMoveRepository>();
+            moveRepositoryMock
+                .Setup(m => m.GetMovesForGame(It.IsAny<string>(), CancellationToken.None))
+                .Returns(() => Task.FromResult(new List<Move>
+                {
+                    new Move
+                    {
+                        GameId = "abc",
+                        BoardPosition = new Position {X = 0, Y = 0},
+                        TilePosition = new Position {X = 0, Y = 0},
+                        Player = Player.Cross
+                    },
+                    new Move
+                    {
+                        GameId = "abc",
+                        BoardPosition = new Position {X = 0, Y = 0},
+                        TilePosition = new Position {X = 1, Y = 0},
+                        Player = Player.Circle
+                    }
+                }))
+                .Verifiable();
+
+            var gameManager = new MoveValidator(moveRepositoryMock.Object);
+
+            // act
+            var result = await gameManager.ValidateMoveAsync(
+                new Move
+                {
+                    GameId = "abc",
+                    BoardPosition = new Position {X = 1, Y = 0},
+                    TilePosition = new Position {X = 0, Y = 0},
+                    Player = Player.Cross
+                }, CancellationToken.None);
+
+            // assert
+            result.Move.MoveNumber.Should().Be(3);
+        }
+    }
+}
