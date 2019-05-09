d836817f21225a563a1be3a27dcee32a1c069751
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-05-09 12:34:17 +0200

    Invalid Board position tests

--- src/Domain.Tests/TicTacToeGameTests.cs
+++ src/Domain.Tests/TicTacToeGameTests.cs
@@ -73,7 +73,113 @@ namespace UltimateTicTacToe.Domain.Tests
 
         #region Invalid Positions
 
-        //TODO: test if it reacts correctly to invalid positions (not on the board)
+        [Theory]
+        [InlineData(-1, 0)]
+        [InlineData(0, -1)]
+        [InlineData(-1, -1)]
+        [InlineData(3, 3)]
+        [InlineData(3, 0)]
+        [InlineData(0, 3)]
+        public void Move_FirstInvalidBoardPositions_InvalidMove(int x, int y)
+        {
+            // arrange
+            var game = new TicTacToeGame(new List<Move>());
+
+            //act
+            var result = game.Move(new Move
+            {
+                Player = Player.Cross,
+                BoardPosition = new Position(x, y),
+                TilePosition = new Position(0, 0),
+                MoveNumber = 1
+            });
+
+            //assert
+            result.IsValid.Should().BeFalse();
+            result.InvalidReason.Should().Be(ExceptionMessages.InvalidPosition);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 1)]
+        [InlineData(0, 2)]
+        [InlineData(1, 0)]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 0)]
+        [InlineData(2, 1)]
+        [InlineData(2, 2)]
+        public void Move_FirstValidBoardPositions_ValidMove(int x, int y)
+        {
+            // arrange
+            var game = new TicTacToeGame(new List<Move>());
+
+            //act
+            var result = game.Move(new Move
+            {
+                Player = Player.Cross,
+                BoardPosition = new Position(x, y),
+                TilePosition = new Position(0, 0),
+                MoveNumber = 1
+            });
+
+            //assert
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(-1, 0)]
+        [InlineData(0, -1)]
+        [InlineData(-1, -1)]
+        [InlineData(3, 3)]
+        [InlineData(3, 0)]
+        [InlineData(0, 3)]
+        public void Move_FirstInvalidTilePositions_InvalidMove(int x, int y)
+        {
+            // arrange
+            var game = new TicTacToeGame(new List<Move>());
+
+            //act
+            var result = game.Move(new Move
+            {
+                Player = Player.Cross,
+                BoardPosition = new Position(0, 0),
+                TilePosition = new Position(x, y),
+                MoveNumber = 1
+            });
+
+            //assert
+            result.IsValid.Should().BeFalse();
+            result.InvalidReason.Should().Be(ExceptionMessages.InvalidPosition);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 1)]
+        [InlineData(0, 2)]
+        [InlineData(1, 0)]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 0)]
+        [InlineData(2, 1)]
+        [InlineData(2, 2)]
+        public void Move_FirstValidTilePositions_ValidMove(int x, int y)
+        {
+            // arrange
+            var game = new TicTacToeGame(new List<Move>());
+
+            //act
+            var result = game.Move(new Move
+            {
+                Player = Player.Cross,
+                BoardPosition = new Position(0, 0),
+                TilePosition = new Position(x, y),
+                MoveNumber = 1
+            });
+
+            //assert
+            result.IsValid.Should().BeTrue();
+        }
 
         #endregion
 
