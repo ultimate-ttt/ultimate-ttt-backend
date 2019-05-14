0961d2dc58351cb95f686b9ba7d72501d5dd900e
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-05-14 14:47:06 +0200

    PositionTests

---
+++ src/Abstractions.Tests/PositionTests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+using Xunit;
+
+namespace UltimateTicTacToe.Abstractions.Tests
+{
+    public class PositionTests
+    {
+        [Theory]
+        [InlineData(0,0,0,0,true)]
+        [InlineData(0,0,0,1,false)]
+        [InlineData(0,0,1,0,false)]
+        [InlineData(0,0,1,1,false)]
+        [InlineData(0,1,0,0,false)]
+        [InlineData(1,0,0,0,false)]
+        [InlineData(1,1,0,0,false)]
+        [InlineData(1,1,1,1,true)]
+        public void Equals_VariousImput_CorrectResult(
+            int p1X,
+            int p1Y,
+            int p2X,
+            int p2Y,
+            bool isEqual)
+        {
+            // arrange
+            var position1 = new Position(p1X, p1Y);
+            var position2 = new Position(p2X, p2Y);
+            //act
+            var result = position1.Equals(position2);
+
+            //assert
+            result.Should().Be(isEqual);
+        }
+    }
+}
