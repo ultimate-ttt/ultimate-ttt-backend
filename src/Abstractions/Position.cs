ef068d24428c71646584773a3537e8d23b092fc6
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-05-15 07:43:37 +0200

    removed unused equality members of position

--- src/Abstractions/Position.cs
+++ src/Abstractions/Position.cs
@@ -31,33 +31,5 @@ namespace UltimateTicTacToe.Abstractions
 
             return X == other.X && Y == other.Y;
         }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj.GetType() != this.GetType())
-            {
-                return false;
-            }
-
-            return Equals((Position)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (X * 397) ^ Y;
-            }
-        }
     }
 }
