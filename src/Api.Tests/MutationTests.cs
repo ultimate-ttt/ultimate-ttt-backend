dfd5ad87cf157868182e15eed4a10a8fbd3e89f0
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-05-22 14:20:12 +0200

    one more test

--- src/Api.Tests/MutationTests.cs
+++ src/Api.Tests/MutationTests.cs
@@ -98,5 +98,51 @@ namespace UltimateTicTacToe.Api.Tests
             //assert
             eventSenderMock.Verify(m => m.SendAsync(It.IsAny<IEventMessage>()), Times.Never);
         }
+
+        [Fact]
+        public async Task Move_validInput_CallsGameManager()
+        {
+            // arrange
+            var moveInput = new MoveInput
+            {
+                GameId = "abcdef",
+                Player = Player.Cross,
+                BoardPosition = new Position(0, 0),
+                TilePosition = new Position(0, 0)
+            };
+
+            var gameManagerMock = new Mock<IGameManager>();
+            gameManagerMock
+                .Setup(m => m.Move(It.IsAny<Move>(), CancellationToken.None))
+                .ReturnsAsync(new MoveResult
+                {
+                    Move = moveInput.ToMove(),
+                    InvalidReason = null,
+                    IsValid = false,
+                    MoveFinishedGame = false,
+                    MoveFinishedBoard = false,
+                });
+
+            var eventSenderMock = new Mock<IEventSender>();
+            eventSenderMock
+                .Setup(m => m.SendAsync(It.IsAny<IEventMessage>()))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            var mutation = new Mutation();
+
+
+            //act
+            var result = await mutation.Move(
+                moveInput,
+                gameManagerMock.Object,
+                eventSenderMock.Object,
+                CancellationToken.None);
+
+            //assert
+            gameManagerMock.Verify(
+                m => m.Move(It.IsAny<Move>(), CancellationToken.None),
+                Times.Once);
+        }
     }
 }
